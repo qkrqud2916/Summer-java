ffc4a381180df5bf9b277c83930d8d7bbe4f82a3
414 <414@DESKTOP-PT50OAD>
2018-07-03 16:39:49 +0900

    구구단

---
+++ src/my/day02/gugu.java
@@ -0,0 +1,20 @@
+package my.day02;
+
+public class gugu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int a=1; a<10; a++){
+			System.out.print(a+"´Ü\n");
+			System.out.println();
+			for(int b = 1; b<10; b++ ){
+						
+			System.out.print(a+"*"+b + "=" + a*b);
+			System.out.println();
+			
+			}
+		}
+
+	}
+
+}
